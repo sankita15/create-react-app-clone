ddf95fc1750b55fcc8057f19288e728e39744efe
Ankita Srivastava <ankitacs0018@gmail.com>
2023-04-22 20:19:47 +0200

    modularized and moved the index-react-multi-component.html file to App.js

---
+++ step-5/App.js
@@ -0,0 +1,7 @@
+const heading1 = React.createElement("h1", {}, "Heading 1")
+const heading2 = React.createElement("h1", {}, "Heading 2")
+const heading3 = React.createElement("h1", {}, "Heading 3")
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render([heading1, heading2, heading3]);
